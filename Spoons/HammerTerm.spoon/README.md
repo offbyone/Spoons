f1e9e1c5185f64ed04bdf4c26ea7e73daecfa429
Chris Rose <offline@offby1.net>
2023-10-17 09:58:52 -0700

    HammerTerm!

---
+++ Spoons/HammerTerm.spoon/README.md
@@ -0,0 +1,5 @@
+# It's HammerTerm
+
+Rather than rely on global hotkeys from my terminal app, this hammerspoon addon does the "right" thing for whatever terminal I'm currently using (at the moment, [WezTerm](https://wezfurlong.org/wezterm/) is my go-to).
+
+By default it's bound to `Cmd-Ctrl-'`
---
+++ Spoons/HammerTerm.spoon/init.lua
@@ -0,0 +1,70 @@
+--- === Terminal Support ===
+---
+--- Add commands to manage terminals
+---
+--- Download: [https://github.com/offbyone/HammerTerm.spoon.zip](https://github.com/offbyone/HammerTerm.spoon.zip)
+
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "HammerTerm"
+obj.version = "0.1"
+obj.author = "Chris Rose <offline@offby1.net>"
+obj.homepage = "https://github.com/offbyone/HammerTerm.spoon"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+local logger = hs.logger.new("HammerTerm")
+
+-- whether to automatically bind keys
+obj.bind = true
+
+-- The default terminal to start
+obj.defaultTerminalAppName = "Wezterm"
+
+-- The terminals to try, in order, before falling back to opening the default
+obj.terminals = {
+  {"Wezterm", "com.github.wez.wezterm"},
+  {"iTerm2", "com.googlecode.iterm2"},
+  {"Terminal", "com.apple.Terminal"},
+}
+
+function obj:bindKeys()
+  hs.hotkey.bind({"cmd", "ctrl"}, "'", obj.toggleTerminal)
+end
+
+function obj:toggleTerminal()
+  local app = nil
+
+  for _, terminal in ipairs(obj.terminals) do
+    local name, bundleId = table.unpack(terminal)
+    local _maybeApp = hs.application.get(bundleId)
+    if _maybeApp ~= nil then
+      app = _maybeApp
+      break
+    else
+      logger.df("Didn't find %s using %s", name, bundleId)
+    end
+  end
+
+  if app ~= nil then
+    if app:isFrontmost() then
+      hs.alert.show(string.format("Hiding %s", app:name()))
+      app:hide()
+    else
+      hs.alert.show(string.format("Foregrounding %s", app:name()))
+      app:activate()
+    end
+  else
+    hs.alert.show(string.format("No active terminal, opening %s", obj.defaultTerminalAppName))
+    hs.application.open(obj.defaultTerminalAppName)
+  end
+end
+
+function obj:start()
+  if self.bind then
+    self:bindKeys()
+  end
+end
+
+return obj
